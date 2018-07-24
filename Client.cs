31c1dce45b8e26fc2eaa9458e38922f63adcc36c
Haridas <haridasnykiel@gmail.com>
2018-07-24 21:18:02 +0100

    Added tests to run the code

--- Client.cs
+++ Client.cs
@@ -1,19 +1,32 @@
 ﻿using System;
+using Xunit;
 
 namespace C__Design_Patterns
 {
     
-    class Client
+    public class Client
     {
-        static void Main(string[] args)
+
+        // static void Main(string[] args)
+        // {
+        //     //Singleton singleton = new Singleton();
+        //     Console.WriteLine("Start Here"); // dotnet 4 
+        //     Singleton instance1 = Singleton.Instance;
+        //     Singleton instance2 = Singleton.Instance;
+        //     instance1.DoSomething();
+        //     Console.WriteLine(instance1 == instance2);
+            
+            
+        // }
+        [Fact]
+        public void Test1() 
         {
-            //Singleton singleton = new Singleton();
-            Console.WriteLine("Start Here"); // dotnet 4 
             Singleton instance1 = Singleton.Instance;
             Singleton instance2 = Singleton.Instance;
+            Assert.Equal(instance1, instance2);
             instance1.DoSomething();
-            Console.WriteLine(instance1 == instance2);
-            
         }
     }
+
+
 }
