c152e7970cab3ade5cb8961758d475a4507e99d8
haridas.nykiel <haridas.nykiel@jet2.com>
2018-09-27 12:45:52 +0100

    Added a bug fix to the Person class tostring overrider method.

--- DecoratorPattern/LoggingComparerTests.cs
+++ DecoratorPattern/LoggingComparerTests.cs
@@ -1,23 +1,17 @@
-using Design_Patterns.Strategy_Pattern;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using Design_Patterns.Strategy_Pattern;
 using Xunit;
 
-
-namespace Design_Patterns.DecoratorPattern
-{
-    public class LoggingComparerTests
-    {
+namespace Design_Patterns.DecoratorPattern {
+    public class LoggingComparerTests {
         [Fact]
-        public void LoggingComparerTest()
-        {
-            var persons = StrategyPattern.people.ToList();
-            //Console.WriteLine("Persons: " + persons.First());
-            persons.Sort(new LoggingComparer<Person>(new AgeComparer()));
+        public void LoggingComparerTest () {
+            var persons = StrategyPattern.people.ToList ();
+            persons.Sort (new LoggingComparer<Person> (new AgeComparer ()));
         }
-
 
     }
 }
--- Strategy Pattern/Person.cs
+++ Strategy Pattern/Person.cs
@@ -1,13 +1,10 @@
-namespace Design_Patterns.Strategy_Pattern
-{
-    public class Person
-    {
+namespace Design_Patterns.Strategy_Pattern {
+    public class Person {
         public string Name { get; set; }
         public int Age { get; set; }
 
-        public override string ToString()
-        {
-            return string.Format("{1} : {2}", Name, Age);
+        public override string ToString () {
+            return string.Format ("{0} : {1}", Name, Age);
         }
     }
 }
